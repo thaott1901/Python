c4f9d9d3eda1d2df60284f067b5bdc3d0b79b024
thaott1901 <thaott1901@gmail.com>
2021-03-21 00:57:41 -0400

    MedianSortedArrays

---
+++ LeetCode/Hard/MedianTwoSortedArrays.py
@@ -0,0 +1,15 @@
+def findMedianSortedArrays(nums1, nums2):
+    nums3 = nums1
+    for num in nums2:
+        nums3.append(num)
+    nums3.sort()
+    mid = len(nums3) // 2
+    if len(nums3) % 2 == 1:
+        return nums3[mid]
+    else:
+        return (nums3[mid] + nums3[mid + 1]) / 2
+
+nums1 = [1,3]
+nums2 = [2,4]
+res = findMedianSortedArrays(nums1, nums2)
+print(res)
