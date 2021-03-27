84ebefb6ab37911564435362baf2355509ab1d72
thaott1901 <thaott1901@gmail.com>
2021-03-27 00:33:48 -0400

    merge sorted arrays

---
+++ LeetCode/Easy/MergeSortedArrays.py
@@ -0,0 +1,7 @@
+def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    """
+    Do not return anything, modify nums1 in-place instead.
+    """
+    for i, v in enumerate(nums2):
+        nums1[m + i] = v
+    nums1.sort()
