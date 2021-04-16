d7fab8ea660ab1eb85800ead71b9de6eb095d56f
thaott1901 <thaott1901@gmail.com>
2021-04-15 22:55:51 -0400

    Climbing stairs

--- LeetCode/Easy/ClimbingStairs.py
+++ LeetCode/Easy/ClimbingStairs.py
@@ -1,11 +1,11 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        s = {}
-        s[0] = 0
-        s[1] = 1
-        s[2] = 2
-        if n <= 2:
-            return n
-        for i in range(3, n+1):
-            s[i] = s[i-1] + s[i-2]
-        return s[n]
+
+def climbStairsn(s: int) -> int:
+    s = {}
+    s[0] = 0
+    s[1] = 1
+    s[2] = 2
+    if n <= 2:
+        return n
+    for i in range(3, n+1):
+        s[i] = s[i-1] + s[i-2]
+    return s[n]
