015265954a8bf819192b263e72b66cdd140c12e3
thaott1901 <thaott1901@gmail.com>
2021-08-11 16:54:53 -0400

    optimize productExceptSelf

--- LeetCode/Medium/ProductArrayExceptSelf.py
+++ LeetCode/Medium/ProductArrayExceptSelf.py
@@ -2,18 +2,32 @@ import numpy as np
 
 
 def productExceptSelf(nums):
-    res = np.ones(len(nums))
-    for i in range(len(nums)):
-        print("-------------")
-        print(res)
-        temp = res[i]
-        res = res * nums[i]
-        res[i] = temp
-        print(res)
-        print("-------------")
+    size = len(nums)
+    index = size - 1
+    # build prefix array (accumulated value before ith element)
+    frontArr = [1] * size
+    for i in range(size):
+        if i == 0:
+            frontArr[i] = 1
+        else:
+            frontArr[i] *= frontArr[i - 1] * nums[i - 1]
+    # build suffix array (accumulated value before ith element)
+    backArr = [1] * size
+    reverse = nums[::-1]
+    for i in range(size):
+        if i == 0:
+            backArr[i] = 1
+        else:
+            backArr[i] *= backArr[i - 1] * reverse[i - 1]
+    # multiply both array element wise
+    res = [1] * size
+    for i in range(size):
+        res[i] = frontArr[i] * backArr[index - i]
+
     return res
 
 
 input = [1, 2, 3, 4]
 output = productExceptSelf(input)
 print(output)
+
