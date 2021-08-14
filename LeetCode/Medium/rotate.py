83cd59cd4d1f76a0134fa14b5702ebad324b87e4
thaott1901 <thaott1901@gmail.com>
2021-08-14 15:03:19 -0400

    rotate

---
+++ LeetCode/Medium/rotate.py
@@ -0,0 +1,11 @@
+def rotate(matrix):
+    matrix.reverse()
+    for i in range(len(matrix)):
+        for j in range(i):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    return matrix
+
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+res = rotate(matrix)
+print(res)
